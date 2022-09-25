294bb109a67da48608d8ec183624bce75ac78efa
Scott-Baldwin <71685500+Scott-Baldwin@users.noreply.github.com>
2022-09-25 17:13:06 -0600

    working scatter function

--- snippets.py
+++ snippets.py
@@ -54,25 +54,42 @@ def FileFolderDialog(method='single',
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-def QuickScatter(x, y, c, cmap=mpl.cm.cool, cmin=0, cmax=1):
-        fig, ax = plt.subplots()
+def QuickScatter(x, y, c, 
+        cmap='jet', 
+        vmin=0, 
+        vmax=1, 
+        ticks=None,
+        xlabel=None,
+        ylabel=None,
+        units='',
+        pad=0.05,
+        ):
 
-        norm = mpl.colors.Normalize(vmin=cmin, vmax=cmax)
+
+        fig, ax = plt.subplots()
+        cmap = mpl.cm[cmap]
+        norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
         sm = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
 
         ax.scatter(x, y, color=sm.cmap(sm.norm(c)))
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
 
-        txt = [str(round(i,2)) for i in c]
+        txt = [str(round(i,1)) for i in c]
         for i,j,k in zip(x,y,txt):
                 ax.annotate(k, xy=(i, j))
 
         cb = plt.colorbar(sm,
+                ax=ax,
                 orientation='vertical',
-                #label='Some Units',
-                #labelpad=-40,
+                ticks=ticks,
+                pad=pad,
                 )
 
-        cb.set_label('Some Units', labelpad=-100)
+        cb.set_label(units, 
+                labelpad=-45,
+                fontsize=10,
+                )
 
         return fig, ax
 
