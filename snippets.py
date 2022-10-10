048c941e99dcbdb39b337bff9700a914aa8dd774
Scott Baldwin <sbaldwin@cppwind.com>
2022-10-10 17:55:28 -0600

    added line to bring file dialog window to front. other minor edits

--- snippets.py
+++ snippets.py
@@ -5,43 +5,41 @@ import os
 import tkinter as tk
 from tkinter import filedialog as fd
 
-def FileFolderDialog(method='single', 
-        ftypes=[], 
-        IncludeAllFiles=True,
-        initdir=os.getcwd(),
+def FileFolderDialog(
+        dir = os.getcwd(),
+        method = 'single', 
+        ftypes = [], 
+        IncludeAllFiles = True,
         ):
-
     # add option for all files
     if IncludeAllFiles: ftypes.append(('All files', '*'))
-
     # create the root window
     root = tk.Tk()
+    root.attributes("-topmost", True) # bring to front
     root.withdraw() # hides root window
-    
     try:
         if method.lower() == 'single': 
                 filename = fd.askopenfilename(
                         parent=root,
                         title='Select a file',
-                        initialdir=initdir,
+                        initialdir=dir,
                         filetypes=ftypes,
                         )
         elif method.lower() == 'multi': 
                 filename = fd.askopenfilenames(
                         parent=root,
                         title='Select file(s)',
-                        initialdir=initdir,
+                        initialdir=dir,
                         filetypes=ftypes,
                         )
         elif method.lower() == 'folder': 
                 filename = fd.askdirectory(
                         parent=root,
                         title='Select a folder',
-                        initialdir=initdir,
+                        initialdir=dir,
                         )
         else: raise TypeError('method not recognized')
         return filename
-
     finally:
         root.destroy()
 
