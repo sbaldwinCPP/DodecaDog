bea3b21f1bc26bfe569ee2e45eb507d3588e90dd
Scott-Baldwin <sbaldwin@cppwind.com>
2023-04-17 18:33:24 -0600

    started a template project for pysimplegui apps

---
+++ psg_app_template/app_main.py
@@ -0,0 +1,19 @@
+# %% imports
+
+import app_module as func
+
+# func.foo()
+# func.foo("test")
+
+
+# import PySimpleGUI as sg
+
+
+# %% functions
+def main():
+    func.foo("do the thing!")
+
+
+# %% run
+if __name__ == "__main__":
+    main()
